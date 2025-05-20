9b958e8a0b0e8e35c7a6fcfb6660cc84327f07ee
ChandraShekhar <csyadav0513@gmail.com>
2025-05-20 14:04:04 +0530

    Add initial migration for Product model and update views for product list handling

--- api/views.py
+++ api/views.py
@@ -2,18 +2,36 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product
 from .serializer import ProductSerializer
+from rest_framework import status
 
 
 @api_view(['GET'])
 def index(request):
     return Response({'message': 'Hello world'})
 
-#! GET ALL /api/products
-@api_view(['GET' , "POST"])
+#! GET ALL and CREATE
+
+
+@api_view(['GET', 'POST'])
 def product_list(request):
-    pass
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():  # ✅ Always check this
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+       
 
 
+
+#! GET ONE, EDIT and DELETE 
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request):
     pass
 
