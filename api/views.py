34da9b1d4e7c2aa6b4ff2a3e43efe49a8dbd600b
ChandraShekhar <csyadav0513@gmail.com>
2025-05-20 11:21:34 +0530

    Add ProductSerializer and update index view to use Response

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-from http import HttpResponse
+from rest_framework.response import Response
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return Response("Hello, world. You're at the polls index.")
     
