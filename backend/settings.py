5712601871ef259380108cde00a6ddea87d43321
ChandraShekhar <csyadav0513@gmail.com>
2025-05-21 09:52:43 +0530

    Fix formatting in INSTALLED_APPS and enable APPEND_SLASH setting

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api",
     "rest_framework",
-    
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+APPEND_SLASH = True
